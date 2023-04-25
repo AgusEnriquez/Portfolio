abe2e698228ec168f0074da38eb7d707b5467dc5
Agustin Enriquez <99294180+AgusEnriquez@users.noreply.github.com>
2023-04-24 23:44:45 -0300

    Volviendo a al ruedo, inicio PortFolio

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+import { Link } from "react-scroll";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="container mx-auto">
+        
+      </div>;
+    </nav>
+  );
+};
+
+export default Nav;
